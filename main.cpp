45045c481817367bd8c73dbb8658339d7a99ee89
safesoc <34695366+safesoc@users.noreply.github.com>
2018-02-08 22:33:11 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*
+* Author: Jacob Wunder
+* Assignment Title: Program 1
+* Assignment Description: Prints a 3-column, 6-row table to console.
+* Due Date: 01/16/18
+* Date Created: 01/15/18
+* Date Last Modified: 01/15/18
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "A        A^2      A^3" << endl;
+    cout << "1        1        1" << endl;
+    cout << "2        4        8" << endl;
+    cout << "3        9        27" << endl;
+    cout << "4        16       64" << endl;
+    cout << "5        25       125" << endl;
+    return 0;
+}
