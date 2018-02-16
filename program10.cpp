6fdc0beda2186b2c65978c2f9bad62f5ecf21adc
Jacob <34695366+safesoc@users.noreply.github.com>
2018-02-15 19:18:03 -0600

    Update program10.cpp

--- program10.cpp
+++ program10.cpp
@@ -11,29 +11,27 @@
     Date Last Modified:     02/14/18
 *******************************************************************************
     Data Abstraction:
-        * int declared for the value used to shift the characters in the file
-        * char declared to use for reading individual characters from file
-        * input and output file streams initialized
-        * strings declared for error message, command input, and file names
-        * stringstream declared to hold the encrypted or decrypted message
-          until ready for printing to screen
+        * int declared for all values except a double for average
+        * input file stream initialized
+        * strings declared for file name and skipping lines.
     Input:
-        * Reads file name and command from the user
+        * Reads file name and  from the user in a loop until a proper file name
+          is read.
     Process:
-        * User input is validated
-        * If input is valid and the file can be opened, the file will be ready
-          and characters will undergo the shift according to the user's
-          choice.
+        * The first two lines of the file are skipped.
+        * Integers are read from the file in a loop until all data is collected
+          and all values are calculated.
     Output:
-        * The encrypted or decrypted message, depending on the user's input,
-          will be printed to the screen and an output file named "message".
+        * All values are printed to the screen
     Assumptions:
         * It is assumed that, having been prompted to do so, the user will
           correctly input data
         * It is assumed that there will be an input file ready for processing
+        * It is assumed that the first two lines will always need to be skipped
 *******************************************************************************
 */
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <string>
 
@@ -46,53 +44,80 @@ int main()
      // DATA ABSTRACTION //
     //////////////////////
 
-    int count = 0;
+    int count = 0, sum = 0;
+    int n, min, max;
+    double avg = 0;
     string inFileName;
+    string skipLine;
     ifstream inFile;
-    
-    
+
+
       ///////////
      // INPUT //
     ///////////
 
 	do
 	{
-    cout << "Enter File Name: ";
-    cin >> inFileName;
-    cout << inFileName << endl;
+        cout << "Enter File Name: ";
+        cin >> inFileName;
+        cout << inFileName << endl;
 
-	inFile.clear();
-    inFile.open(inFileName.c_str());
-	
-    if(!inFile)
-    {
-        cout << "Error: File did NOT open." << endl;
-    }
-	while {!infile);
-	
-	
+        inFile.clear();
+        inFile.open(inFileName.c_str());
+
+        if(!inFile)
+        {
+            cout << "Error: File Not Open.\n" << endl;
+        }
+	}
+	while (!inFile);
+
+
       /////////////
      // PROCESS //
     /////////////
 
-    while (inFile.get(c))
+    while(inFile)
     {
-        c += shift;
-        ss << c;
+        getline(inFile,skipLine);
+        getline(inFile,skipLine);
+
+        min = n;
+        max = n;
+
+        while (inFile >> n)
+        {
+            count++;
+            sum += n;
+
+            if (n > max)
+            {
+                max = n;
+            }
+
+            if (n < min)
+            {
+                min = n;
+            }
+
+        }
     }
 
+    avg = static_cast<double>(sum) / count;
 
 
       ////////////
      // OUTPUT //
     ////////////
 
-    cout << ss.str() << endl;
-    outFile << ss.str() << endl;
+    cout << setprecision(3) << fixed << endl;
+    cout << "Min = " << min << endl;
+    cout << "Max = " << max << endl;
+    cout << "Sum = " << sum << endl;
+    cout << "Count = " << count << endl;
+    cout << "Average = " << avg << endl;
 
-
-    outFile.close();
     inFile.close();
-    
+
     return 0;
 }
